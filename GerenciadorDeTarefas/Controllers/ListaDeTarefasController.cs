62cdf0fa39c62da9b23457dd7f678412bfb076e0
Dani Santos <dani_cs01@hotmail.com>
2019-08-29 18:05:47 -0300

    Add Alteracoes

--- GerenciadorDeTarefas/Controllers/ListaDeTarefasController.cs
+++ GerenciadorDeTarefas/Controllers/ListaDeTarefasController.cs
@@ -26,36 +26,53 @@ namespace GerenciadorDeTarefas.Controllers
 
         public async Task<IActionResult> Index()
         {
-            var lista = await _context.ListaDeTarefas.ToListAsync();           
+            var lista = await _context.ListaDeTarefas.AsNoTracking().ToListAsync();           
             return View(_mapper.Map<List<ListaDeTarefaViewModel>>(lista));
         }
 
         [HttpGet]
         public IActionResult Create()
         {
+            ViewData["listas"] = _context.ListaDeTarefas.AsNoTracking().ToList();
             return View();
         }
 
         [HttpPost]
         public async Task<IActionResult> Create(ListaDeTarefaViewModel listaDeTarefaviewModel)
         {
-            ViewData["listas"] = _context.ListaDeTarefas.ToList();
+            ViewData["listas"] = _context.ListaDeTarefas.AsNoTracking().ToList();
             var lista = _mapper.Map<ListaDeTarefa>(listaDeTarefaviewModel);
             await _context.ListaDeTarefas.AddAsync(lista);
             await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
+            return RedirectToAction("Edit", new { id = lista.Id });
         }
 
         public IActionResult Edit(int Id)
         {
-            ViewData["listas"] = _context.ListaDeTarefas.ToList();
-            var tarefas = _context.ListaDeTarefas.Include(x => x.Tarefas).Where(x => x.Id == Id).FirstOrDefault();           
+            ViewData["listas"] = _context.ListaDeTarefas.AsNoTracking().ToList();
+            var tarefas = _context.ListaDeTarefas.Include(x => x.Tarefas).AsNoTracking().Where(x => x.Id == Id).FirstOrDefault();           
             if(tarefas.Tarefas == null)
             {
                 tarefas.Tarefas = new List<Tarefa>();               
-            } 
-            
+            }             
             return View(_mapper.Map<ListaDeTarefaViewModel>(tarefas));
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int Id)
+        {
+            ViewData["listas"] = _context.ListaDeTarefas.AsNoTracking().ToList();
+            var listadetarefa = _context.ListaDeTarefas.Find(Id);
+            return View(_mapper.Map<ListaDeTarefaViewModel>(listadetarefa));
+        }
+
+        [HttpPost]
+        public IActionResult Delete(ListaDeTarefaViewModel listaDeTarefaViewModel)
+        {
+            ViewData["listas"] = _context.ListaDeTarefas.AsNoTracking().ToList();
+            _context.ListaDeTarefas.Remove(_mapper.Map<ListaDeTarefa>(listaDeTarefaViewModel));
+            _context.SaveChanges();
+            return RedirectToAction("Edit", new { id = 1 });
         }
     }
 }
